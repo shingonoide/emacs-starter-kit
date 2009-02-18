8cd70e97c4050b4a252a517dbc29cf092218c8c1
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-17 22:41:03 -0800

    Added desktop configuration with the emacs server

---
+++ desktop.el
@@ -0,0 +1,14 @@
+;; emacsclient
+
+; Finish editing on a client
+(global-set-key (kbd "C-c x") 'server-edit)
+
+(add-hook 'server-switch-hook
+          (lambda nil
+            (let ((server-buf (current-buffer)))
+              (bury-buffer)
+              (switch-to-buffer-other-frame server-buf))))
+
+(add-hook 'server-done-hook 'delete-frame)
+
+(server-start)
