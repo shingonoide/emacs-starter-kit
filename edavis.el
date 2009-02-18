802f8015c30626948260b0d0cab7fcc1e6b46cef
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-18 01:10:54 -0800

    Added my colection of tweaks

--- edavis.el
+++ edavis.el
@@ -2,3 +2,55 @@
 ; http://www.cs.cmu.edu/~maverick/GNUEmacsColorThemeTest/
 (load "color-theme-library")
 (color-theme-lss)
+
+; Re-read a buffer from disk
+(global-set-key (kbd "<f5>") 'revert-buffer)
+
+; Search tags
+(global-set-key [(control tab)] 'find-tag)
+
+; Indent with spaces
+(setq-default indent-tabs-mode nil)
+
+; Set current tabs to 2
+(setq default-tab-width 4)
+
+; Syntax highlight please
+(global-font-lock-mode t)
+
+; Spel check comments
+(setq-default flyspell-prog-mode t)
+
+; Magic Function to chmod +x anyfile that starts with a hashbang
+; http://rubygarden.org/ruby?InstallingEmacsExtensions
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+; No scroll please
+(scroll-bar-mode -1)
+
+; format the title-bar to always include the buffer name
+(setq frame-title-format (list "" "emacs" ": %f" ))
+
+(setq auto-mode-alist (cons '("\\.mdwn$" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.page$" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.eml$" . markdown-mode) auto-mode-alist))
+
+;; Tab completion
+;; http://www.emacsblog.org/2007/03/12/tab-completion-everywhere/
+(global-set-key [(tab)] 'smart-tab)
+(defun smart-tab ()
+  "This smart tab is minibuffer compliant: it acts as usual in
+    the minibuffer. Else, if mark is active, indents region. Else if
+    point is at the end of a symbol, expands it. Else indents the
+    current line."
+  (interactive)
+  (if (minibufferp)
+      (unless (minibuffer-complete)
+        (dabbrev-expand nil))
+    (if mark-active
+        (indent-region (region-beginning)
+                       (region-end))
+      (if (looking-at "\\_>")
+          (dabbrev-expand nil)
+        (indent-for-tab-command)))))
+
