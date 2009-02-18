a2bef2f207d1dc6f57d4a7a35ee6f5ca2e6c3ad2
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-18 01:09:34 -0800

    Make sure the emacs server cleans up it's socket file when exiting

--- desktop.el
+++ desktop.el
@@ -11,4 +11,26 @@
 
 (add-hook 'server-done-hook 'delete-frame)
 
-(server-start)
+; Remove the server socket when emacs exits.  Had to shell out because
+; delete-file was returning "/tmp/emacs1000/server: No such device or address"
+;
+; Based on code from:
+;
+; http://github.com/stsquad/my-emacs-stuff/blob/af57d1707881b23f05bb89dc926dc8a2b08addb3/my-emacs-server.el
+;
+(defun is-server-running ()
+  "Check is an emacs-server process is already running"
+  (let ((socket-path (concat server-socket-dir "/server")))
+    (file-exists-p socket-path)))
+
+(defun my-server-kill-emacs-hook ()
+  "Clean up server files when we exit"
+  (if (is-server-running)
+      (call-process "/bin/rm" nil nil nil (concat server-socket-dir "/server"))))
+
+(add-hook 'kill-emacs-hook
+          (lambda ()
+            (my-server-kill-emacs-hook)))
+
+(unless (is-server-running)
+  (server-start))
