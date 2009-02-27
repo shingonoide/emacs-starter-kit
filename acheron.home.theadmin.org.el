39dee12e3d6c979f0a3df05e5bf8e6d75fab6549
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-26 21:49:18 -0800

    Smaller font by default

--- acheron.home.theadmin.org.el
+++ acheron.home.theadmin.org.el
@@ -1,5 +1,5 @@
 ;; Common desktop settings
 (load "desktop.el")
 
-(set-default-font "Bitstream Vera Sans Mono-14")
-(add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-14"))
+(set-default-font "Bitstream Vera Sans Mono-12")
+(add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-12"))
