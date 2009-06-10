dd22fbd23d62cbd6266ec8d66d4d3e7f951128a5
Rui Andrada <randrada@barradev.com>
2009-06-10 13:34:46 -0300

    Mais testes de outras funcionalidades no emacs

--- shingo.el
+++ shingo.el
@@ -41,6 +41,41 @@
 (setq auto-mode-alist (cons '("\\.page$" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.eml$" . markdown-mode) auto-mode-alist))
 
+
+;; Load CEDET.
+;; See cedet/common/cedet.info for configuration details.
+;; (load-file "~/elisp/cedet-1.0pre6/common/cedet.el")
+
+
+;; Enable EDE (Project Management) features
+;; (global-ede-mode 1)
+
+;; Enable EDE for a pre-existing C++ project
+;; (ede-cpp-root-project "NAME" :file "~/myproject/Makefile")
+
+
+;; Enabling Semantic (code-parsing, smart completion) features
+;; Select one of the following:
+
+;; * This enables the database and idle reparse engines
+;; (semantic-load-enable-minimum-features)
+
+;; * This enables some tools useful for coding, such as summary mode
+;;   imenu support, and the semantic navigator
+;; (semantic-load-enable-code-helpers)
+
+;; * This enables even more coding tools such as intellisense mode
+;;   decoration mode, and stickyfunc mode (plus regular code helpers)
+;; (semantic-load-enable-gaudy-code-helpers)
+
+;; * This enables the use of Exuberent ctags if you have it installed.
+;;   If you use C++ templates or boost, you should NOT enable it.
+;; (semantic-load-enable-all-exuberent-ctags-support)
+
+;; Enable SRecode (Template management) minor-mode.
+;; (global-srecode-minor-mode 1)
+
+
 ;; Tab completion
 ;; http://www.emacsblog.org/2007/03/12/tab-completion-everywhere/
 (global-set-key [(tab)] 'smart-tab)
@@ -64,12 +99,28 @@
 (add-to-list 'auto-mode-alist '("\\.liquid$" . nxhtml-mode))
 (setenv "ESHELL" (expand-file-name "~/bin/eshell"))
 
-;; geben debug
-(add-to-list 'load-path "/Users/shingo/elisp/geben")
-(add-to-list 'load-path "/Users/shingo/elisp/geben/gud")
-
 ;; shell-mode
 (defun shell ()
   (interactive)
   (ansi-term "/bin/zsh"))
-    
+
+;; geben debug
+(add-to-list 'load-path "/Users/shingo/elisp/geben")
+(add-to-list 'load-path "/Users/shingo/elisp/geben/gud")
+
+
+(autoload 'geben "geben" "PHP Debugger on Emacs" t)
+
+;; (load-file "~/.emacs.d/elpa/emms-3.0/emms.el")
+;; (require 'emms-setup) (emms-devel) (emms-default-players)
+
+;; (global-set-key (kbd "C-c <up>") 'emms-start)
+;; (global-set-key (kbd "C-c <down>") 'emms-stop)
+;; (global-set-key (kbd "C-c <left>") 'emms-previous)
+;; (global-set-key (kbd "C-c <right>") 'emms-next)
+
+
+;; (add-to-list 'load-path "~/elisp/ecb/")
+;; (load-file "~/elisp/ecb/ecb.el")
+;; (require 'ecb)
+
