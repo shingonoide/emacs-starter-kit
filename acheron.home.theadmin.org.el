1a9dc614b20cb0457a801958ef062e7785f454a0
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-25 10:09:56 -0800

    New font

--- acheron.home.theadmin.org.el
+++ acheron.home.theadmin.org.el
@@ -1,5 +1,7 @@
 ;; Common desktop settings
 (load "desktop.el")
 
-(set-default-font "Bitstream Vera Sans Mono-11")
-(add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-11"))
+;; (set-default-font "Bitstream Vera Sans Mono-11")
+;; (add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-11"))
+(set-default-font "Inconsolata-13")
+(add-to-list 'default-frame-alist '(font . "Inconsolata-13"))
