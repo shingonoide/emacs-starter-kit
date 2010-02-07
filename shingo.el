38fc8e2129b9884adf01badcc12d253a8c1c666c
Rui Andrada <randrada@barradev.com>
2010-02-07 19:05:26 -0200

    Algumas modificações para ficar mais compatível

--- shingo.el
+++ shingo.el
@@ -30,9 +30,13 @@
 
 ;; Always wordwrap
 (setq truncate-partial-width-windows nil)
+(setq word-wrap t)
 
 ; no autofills please
 (setq auto-fill-mode 0)
+
+; Always highlight the current line
+(global-hl-line-mode 1)
 
 ; format the title-bar to always include the buffer name
 (setq frame-title-format (list "" "emacs" ": %f" ))
@@ -40,7 +44,90 @@
 (setq auto-mode-alist (cons '("\\.mdwn$" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.page$" . markdown-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.eml$" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.md$" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.markdown$" . markdown-mode) auto-mode-alist))
 
+;; Tab completion
+;; http://www.emacsblog.org/2007/03/12/tab-completion-everywhere/
+(global-set-key [(tab)] 'smart-tab)
+(defun smart-tab ()
+  "This smart tab is minibuffer compliant: it acts as usual in
+    the minibuffer. Else, if mark is active, indents region. Else if
+    point is at the end of a symbol, expands it. Else indents the
+    current line."
+  (interactive)
+  (if (minibufferp)
+      (unless (minibuffer-complete)
+        (dabbrev-expand nil))
+    (if mark-active
+        (indent-region (region-beginning)
+                       (region-end))
+      (if (looking-at "\\_>")
+          (dabbrev-expand nil)
+        (indent-for-tab-command)))))
+
+
+(add-to-list 'auto-mode-alist '("\\.liquid$" . nxhtml-mode))
+
+
+(autoload 'todo-list-mode "todo-list-mode") ;load when needed
+ 
+;a simple function that opens the file,
+;and switches to todo-list-mode.
+(defun open-todo-list ()
+  (interactive)
+  (find-file "~/TODO")
+  (todo-list-mode))
+ 
+;then bind to control-f12 so i can call it with one keystroke
+;this works well for me because i also bind calendar to f12
+;(global-set-key [C-f12] 'open-todo-list)
+
+(setq auto-mode-alist (cons '("\\.todo$" . todo-list-mode) auto-mode-alist))
+
+;; Time inserts
+(defun insert-time ()
+  "2007-09-03-20:54"
+  (interactive)
+  (insert (format-time-string "%Y-%m-%d-%R")))
+
+;; ISO time
+(defun insert-time-ISO ()
+  "2007-09-03"
+  (interactive)
+  (insert (format-time-string "%Y-%m-%d")))
+
+;; Emacs macro to sort the todo list
+(fset 'sort-todo
+      [?\M-< ?\C-  ?\M-> ?\M-x ?s ?o ?r ?t ?- ?l ?i ?n ?e ?s return ?\M-<])
+
+;; Build tags automatically
+(setq rinari-tags-file-name "TAGS")
+
+;; Unbind arrow keys to learn the emacs movement keys better
+(global-set-key [up] nil)
+(global-set-key [down] nil)
+(global-set-key [left] nil)
+(global-set-key [right] nil)
+
+(setenv "ESHELL" (expand-file-name "~/bin/eshell"))
+
+;; shell-mode
+(defun shell ()
+  (interactive)
+  (ansi-term "/bin/zsh"))
+
+(setq ruby-insert-encoding-magic-comment nil)
+
+(load-file "~/Repositories/Terceiros/cucumber.el/feature-mode.el")
+
+;; Rinari
+(add-to-list 'load-path "~/.emacs.d/rinari")
+(require 'rinari)
+
+;; geben debug
+;; (add-to-list 'load-path "/Users/shingo/elisp/geben")
+;; (add-to-list 'load-path "/Users/shingo/elisp/geben/gud")
 
 ;; Load CEDET.
 ;; See cedet/common/cedet.info for configuration details.
@@ -75,49 +162,8 @@
 ;; Enable SRecode (Template management) minor-mode.
 ;; (global-srecode-minor-mode 1)
 
-
-;; Tab completion
-;; http://www.emacsblog.org/2007/03/12/tab-completion-everywhere/
-(global-set-key [(tab)] 'smart-tab)
-(defun smart-tab ()
- "This smart tab is minibuffer compliant: it acts as usual in
-   the minibuffer. Else, if mark is active, indents region. Else if
-   point is at the end of a symbol, expands it. Else indents the
-   current line."
- (interactive)
- (if (minibufferp)
-     (unless (minibuffer-complete)
-       (dabbrev-expand nil))
-   (if mark-active
-       (indent-region (region-beginning)
-                      (region-end))
-     (if (looking-at "\\_>")
-         (dabbrev-expand nil)
-       (indent-for-tab-command)))))
-
-
-(add-to-list 'auto-mode-alist '("\\.liquid$" . nxhtml-mode))
-(setenv "ESHELL" (expand-file-name "~/bin/eshell"))
-
-;; shell-mode
-(defun shell ()
-  (interactive)
-  (ansi-term "/bin/zsh"))
-
-(setq ruby-insert-encoding-magic-comment nil)
-
-(load-file "~/Repositories/Terceiros/cucumber.el/feature-mode.el")
-
-;; Rinari
-(add-to-list 'load-path "~/.emacs.d/rinari")
-(require 'rinari)
-
-;; geben debug
-;; (add-to-list 'load-path "/Users/shingo/elisp/geben")
-;; (add-to-list 'load-path "/Users/shingo/elisp/geben/gud")
-
-
 ;; (autoload 'geben "geben" "PHP Debugger on Emacs" t)
+
 
 ;; (load-file "~/.emacs.d/elpa/emms-3.0/emms.el")
 ;; (require 'emms-setup) (emms-devel) (emms-default-players)
@@ -126,7 +172,6 @@
 ;; (global-set-key (kbd "C-c <down>") 'emms-stop)
 ;; (global-set-key (kbd "C-c <left>") 'emms-previous)
 ;; (global-set-key (kbd "C-c <right>") 'emms-next)
-
 
 ;; (add-to-list 'load-path "~/elisp/ecb/")
 ;; (load-file "~/elisp/ecb/ecb.el")
